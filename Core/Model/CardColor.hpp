51194dcd8a0c8a7dd73dc2a7fa1a07d4824023a0
Bejenar Nicolai <admynyral2808@gmail.com>
2024-09-09 17:38:35 +0300

    Lab #1

---
+++ Core/Model/CardColor.hpp
@@ -0,0 +1,9 @@
+﻿#pragma once
+
+enum class CardColor {
+    WHITE = 1,
+    RED = 2,
+    BLUE = 3,
+    YELLOW = 4,
+    GREEN = 5
+};
---
+++ Core/Model/Player.hpp
@@ -0,0 +1,26 @@
+﻿#ifndef PLAYER_HPP
+#define PLAYER_HPP
+#include <string>
+#include "Card.hpp"
+
+
+class Player {
+public:
+    int id;
+    std::string name;
+    Card* hand[5];
+
+    explicit Player(const std::string &name);
+    ~Player();
+
+    std::string handToString() const;
+
+    static std::string handToString(Card* const hand[5]);
+
+private:
+    static int nextId;
+    static const int MAX_HAND = 5;
+};
+
+
+#endif //PLAYER_HPP
