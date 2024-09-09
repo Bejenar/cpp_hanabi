0d53b921e8db0f474ea3419c2fa194f9e773ef3f
Bejenar Nicolai <admynyral2808@gmail.com>
2024-09-09 17:38:35 +0300

    Lab #1

---
+++ Core/Model/Card.hpp
@@ -0,0 +1,24 @@
+﻿#ifndef CARD_HPP
+#define CARD_HPP
+#include <string>
+
+#include "CardColor.hpp"
+
+
+class Card {
+public:
+    int id;
+    int rank;
+    CardColor color;
+
+    Card(int rank, CardColor color);
+
+    std::string toString() const;
+
+private:
+    static int nextId;
+    static std::string colorToString(CardColor color);
+};
+
+
+#endif //CARD_HPP
